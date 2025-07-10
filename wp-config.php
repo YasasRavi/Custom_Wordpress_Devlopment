25a9c10ac7d2c6dddc6d2d1cd82829ab51e8be5a
Yasas Ravihara <>
2025-07-10 13:40:59 +0530

    ok

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'bizycorp' );
+define( 'DB_NAME', 'custom_wp_dev' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
